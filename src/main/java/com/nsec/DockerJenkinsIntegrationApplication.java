23a39df3b7c8df1261edcb50727f02d43576c1d3
sknsec <subhash.nsec@gmail.com>
2021-09-14 15:45:25 +0530

    3rd commit

--- src/main/java/com/nsec/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/nsec/DockerJenkinsIntegrationApplication.java
@@ -2,13 +2,21 @@ package com.nsec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication
+@SpringBootApplication
+@RestController
 public class DockerJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello subhash....");
-		//SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+
+	@GetMapping(value = "/message")
+	public String getMsg()
+	{
+		return "Hello Subhash.....";
 	}
 
 }
